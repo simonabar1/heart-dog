a06b67455ac798023d0c028d7c9cf8f5aa5a7059
Simona Barone <simona_barone@hotmail.com>
2023-01-31 18:47:20 +0100

    fixing production errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  # devise_for :users
+  devise_for :users
   resources :users, only: [:show, :edit, :update]
   resources :organizations
   resources :dogs

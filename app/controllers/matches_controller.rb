eb89126ac29877de9336833dcb88f6fffb2c70f6
Simona Barone <simona_barone@hotmail.com>
2023-02-02 18:32:26 +0100

    Fixed displaying issues due to additional end

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -3,34 +3,14 @@ class MatchesController < ApplicationController
   def index
     # Setting the user
     @user = current_user
-
-    # Setting the age categories
-     @age = Date.today.year - Dog.find(params[:id]).birthday.year
-     if @age <= 1
-      age_category == 'Puppy'
-     elsif @age > 1 && @age < 7
-      age_category == 'Adult'
-     else
-      age_category == 'Senior'
-
+    @dogs = Dog.where(adopted: false)
     @new_dogs = []
-
-    # Setting an array of dogs matching the user criteria
     @dogs.each do |dog|
-
-      # Select only dogs that weren't already liked or disliked
 
       if dog.liked?(current_user) == false && dog.disliked?(current_user) == false
         @new_dogs.push(dog)
       end
 
-      # Selecting dogs that weren't adopted yet
-
-      if dog.adopted?
-        @new_dogs.delete(dog)
-      end
-
-      # Selecting dog by criteria - gender, vaccination, neutered, age range
       if dog.gender != @user.user_preference.gender && @user.user_preference.gender != nil
         @new_dogs.delete(dog)
       elsif
@@ -39,7 +19,7 @@ class MatchesController < ApplicationController
       elsif
       dog.neutered != @user.user_preference.neutered && @user.user_preference.neutered != nil
         @new_dogs.delete(dog)
-      elsif
+      end
       @dog = @new_dogs.sample
   end
 end
